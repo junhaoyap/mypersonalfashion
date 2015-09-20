8a0d0e3abf323e487a47d56af15c978e9583b54e
junhaoyap <lordyyap@gmail.com>
2015-09-20 03:34:17 -0700

    Resolve conflicts

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,6 +1,5 @@
 Template.home.rendered = function() {
 	console.log(Meteor.user());
-  $('#more').click(function() {
   $('#requestButton').click(function() {
     if (Meteor.user()) {
 			Meteor.users.update({ _id: Meteor.userId() }, { $set: { 'profile.isWantAdvice': true }});
@@ -28,11 +27,9 @@ Template.home.rendered = function() {
 	}
 };
 
-
 Template.home.helpers({
 	profiles: function() {
 	    var objects = Meteor.users.find({'profile.isWantAdvice': true}).fetch();
 	    return objects;
 	}
 });
-
