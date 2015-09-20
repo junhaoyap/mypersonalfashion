10f621ead869fe8095df85a764162b2a1181bdb0
junhaoyap <lordyyap@gmail.com>
2015-09-20 05:03:39 -0700

    Remove nonsense

--- both/router/routes.js
+++ both/router/routes.js
@@ -2,14 +2,6 @@ Router.route('/', {
   name: 'home'
 });
 
-Router.route('/dashboard', {
-  name: 'dashboard'
-});
-
-Router.route('/userProfile', {
-  name: 'userProfile'
-});
-
 Router.route('likedItems', {
   name: 'likedItems'
 });
@@ -18,19 +10,10 @@ Router.route('recommendations', {
   name:'recommendations'
 });
 
-
-Router.route('/items/new', {
-  name: 'items.new'
-});
-
 Router.route('/selectbrands', {
   name: 'selectBrands'
 });
 
 Router.route('/feed', {
   name: 'feed'
-});
-
-Router.plugin('ensureSignedIn', {
-  only: ['dashboard']
 });
