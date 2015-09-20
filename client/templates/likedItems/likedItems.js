3fd820860b5e75adbf654a5bdab8b95e0eb3dee3
junhaoyap <lordyyap@gmail.com>
2015-09-20 02:46:16 -0700

    More blobs of shit

--- client/templates/likedItems/likedItems.js
+++ client/templates/likedItems/likedItems.js
@@ -1,4 +1,10 @@
 Template.likedItems.rendered = function() {
-  var userId = Router.current().params.query._id;
   // get liked items associated with the id and display them in the html
 }
+
+Template.likedItems.helpers({
+  userToShow: function() {
+    var userId = Router.current().params.query._id;
+    return Meteor.users.findOne({ _id: userId});
+  }
+});
