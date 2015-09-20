36bfd13efd3e5273ff6645f19f3fe2c62003be34
junhaoyap <lordyyap@gmail.com>
2015-09-20 08:42:22 -0700

    Add redirect

--- server/global.js
+++ server/global.js
@@ -1,5 +1,3 @@
-
-
 Accounts.onCreateUser(function(options, user) {
   if (options.profile) {
     options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?type=large";
@@ -7,3 +5,13 @@ Accounts.onCreateUser(function(options, user) {
   }
   return user;
 });
+
+REDIRECT_URL = 'http://iamfabulous.club';
+
+WebApp.connectHandlers
+  .use(function(req, res, next) {
+    console.log(req.url);
+    var location = REDIRECT_URL;
+    res.writeHead(301, {'Location': location});
+    res.end();
+  });
