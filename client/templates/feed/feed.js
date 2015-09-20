a8b2fd35ac3be7c93b6af06c487df768564d1dd3
lareinating <lareinating@gmail.com>
2015-09-20 03:00:02 -0700

    more ui change

--- client/templates/feed/feed.js
+++ client/templates/feed/feed.js
@@ -10,7 +10,19 @@ Template.feed.rendered = function() {
     $('#products-feed').html('');
     for (var i = 0; i < products.length; i ++) {
       var product = products[i];
-      $('#products-feed').append('<div><img src="'+product.media.images[0].smallHdUrl+'"/><p class="product-name truncate">'+product.name+'</p></div>');
+      $('#products-feed').append('<div class="card-wrapper"><img src="'+
+                                  product.media.images[0].smallHdUrl+
+                                  '"/><p class="product-name truncate">'
+                                  +product.name+'</p></div>');
+      // '<div class="card-wrapper">' + 
+      //             '<div class="brand-img banner-bottom-info"></div>' +
+      //             '<div class="banner-bottom-info">' +
+      //             '<p class=" brand-name truncate"></p></div>' +
+      //             '<div class="brand-control">' +
+      //             '<span class="button-dislike"><i class="fa fa-times fa"></i></span>' + 
+      //             '<span class="button-like"><i class="fa fa-heart fa"></i></span>' +
+      //           '</div>' +
+
     }
   }
 };
