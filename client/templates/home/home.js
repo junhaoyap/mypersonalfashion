69e12e5671cb480c471586fb2d83d0a8492afc01
junhaoyap <lordyyap@gmail.com>
2015-09-20 04:14:55 -0700

    Fix header and home after sign up

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,5 +1,4 @@
 Template.home.rendered = function() {
-	console.log(Meteor.user());
   $('#requestButton').click(function() {
     if (Meteor.user()) {
 			Meteor.users.update({ _id: Meteor.userId() }, { $set: { 'profile.isWantAdvice': true }});
