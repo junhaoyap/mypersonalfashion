cd7456d6dc1430319c9c14342a47e023a228b0d7
junhaoyap <lordyyap@gmail.com>
2015-09-20 06:20:11 -0700

    Feed slightly up and fixes

--- client/templates/feed/feed.js
+++ client/templates/feed/feed.js
@@ -1,4 +1,12 @@
 Template.feed.rendered = function() {
+  var userId = Router.current().params.query._id;
+  $.post('http://37cee33b.ngrok.com/brands/unreviewed', {user_id: userId}, function(result) {
+    var brandId = result.brand_id;
+    $.get("https://api.zalando.com/articles?brand=" + brandId + "&pageSize=20&sort=popularity", function(data) {
+       updateProducts(data.content);
+     });
+  });
+
   function updateProducts(products){
     $('#products-feed').html('');
     for (var i = 0; i < products.length; i ++) {
@@ -9,6 +17,9 @@ Template.feed.rendered = function() {
                                   +product.name+'</p></div>');
     }
   }
+  // <!-- Show items that were requested to be commented on -->
+  // <!-- Comment Box -> I think you will look freaking awesome in this! -->
+  // <!-- Submit Button -->
 };
 
 Template.feed.helpers({
